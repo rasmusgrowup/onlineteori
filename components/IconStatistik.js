184d135f92d0612ea300c17709617a0b0afc9141
Rasmus Andersen <rasmus@growupstudio.dk>
2022-05-04 13:39:04 +0200

    first commit

--- components/IconStatistik.js
+++ components/IconStatistik.js
@@ -4,8 +4,8 @@ export default function IconStatistik() {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="10.153" height="10.148" viewBox="0 0 10.153 10.148">
       <g id="Icon_feather-pie-chart" data-name="Icon feather-pie-chart" transform="translate(-2.477 -2.5)">
-        <path id="Path_11" data-name="Path 11" d="M11.769,10.207A4.565,4.565,0,1,1,5.738,4.245" transform="translate(0 -0.866)" fill="none" stroke="#001bad" strokeLinecap="round" strokeLinejoin="round" stroke-width="1"/>
-        <path id="Path_12" data-name="Path 12" d="M22.565,7.565A4.565,4.565,0,0,0,18,3V7.565Z" transform="translate(-10.436)" fill="none" stroke="#001bad" strokeLinecap="round" strokeLinejoin="round" stroke-width="1"/>
+        <path id="Path_11" data-name="Path 11" d="M11.769,10.207A4.565,4.565,0,1,1,5.738,4.245" transform="translate(0 -0.866)" fill="none" stroke="#001bad" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1"/>
+        <path id="Path_12" data-name="Path 12" d="M22.565,7.565A4.565,4.565,0,0,0,18,3V7.565Z" transform="translate(-10.436)" fill="none" stroke="#001bad" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1"/>
       </g>
     </svg>
   )
