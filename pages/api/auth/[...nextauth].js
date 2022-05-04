e2b885b6ec0f09c62b07eac15465171f675a1caf
Rasmus Andersen <rasmus@growupstudio.dk>
2022-05-04 13:58:15 +0200

    first commit

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -69,7 +69,7 @@ export default NextAuth({
   ],
   callbacks: {
     async redirect({ url, baseUrl }) {
-      return `${baseUrl}/account`
+      return `${process.env.NEXTAUTH_URL}/account`
     },
     async session({ session, token }) {
       session.userId = token.sub;
