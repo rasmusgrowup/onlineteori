77df7c629da5442e7aa59b0efbfd5a4bd77ded3f
Rasmus Andersen <rasmus@growupstudio.dk>
2022-05-04 13:35:05 +0200

    first commit

--- components/IconBegivenheder.js
+++ components/IconBegivenheder.js
@@ -4,10 +4,10 @@ export default function IconBegivenheder() {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="9.161" height="10.067" viewBox="0 0 9.161 10.067">
       <g id="Icon_feather-calendar" data-name="Icon feather-calendar" transform="translate(-4 -2.5)">
-        <path id="Path_19" data-name="Path 19" d="M5.407,6h6.347a.907.907,0,0,1,.907.907v6.347a.907.907,0,0,1-.907.907H5.407a.907.907,0,0,1-.907-.907V6.907A.907.907,0,0,1,5.407,6Z" transform="translate(0 -2.093)" fill="none" stroke="#001bad" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/>
-        <path id="Path_20" data-name="Path 20" d="M24,3V4.813" transform="translate(-13.606)" fill="none" stroke="#001bad" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/>
-        <path id="Path_21" data-name="Path 21" d="M12,3V4.813" transform="translate(-5.233)" fill="none" stroke="#001bad" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/>
-        <path id="Path_22" data-name="Path 22" d="M4.5,15h8.161" transform="translate(0 -8.373)" fill="none" stroke="#001bad" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/>
+        <path id="Path_19" data-name="Path 19" d="M5.407,6h6.347a.907.907,0,0,1,.907.907v6.347a.907.907,0,0,1-.907.907H5.407a.907.907,0,0,1-.907-.907V6.907A.907.907,0,0,1,5.407,6Z" transform="translate(0 -2.093)" fill="none" stroke="#001bad" strokeLinecap="round" strokeLinejoin="round" stroke-width="1"/>
+        <path id="Path_20" data-name="Path 20" d="M24,3V4.813" transform="translate(-13.606)" fill="none" stroke="#001bad" strokeLinecap="round" strokeLinejoin="round" stroke-width="1"/>
+        <path id="Path_21" data-name="Path 21" d="M12,3V4.813" transform="translate(-5.233)" fill="none" stroke="#001bad" strokeLinecap="round" strokeLinejoin="round" stroke-width="1"/>
+        <path id="Path_22" data-name="Path 22" d="M4.5,15h8.161" transform="translate(0 -8.373)" fill="none" stroke="#001bad" strokeLinecap="round" strokeLinejoin="round" stroke-width="1"/>
       </g>
     </svg>
   )
