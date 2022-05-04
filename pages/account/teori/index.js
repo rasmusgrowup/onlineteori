7b0d32c7b7d24ad694552343243c58d5b193c01d
Rasmus Andersen <rasmus@growupstudio.dk>
2022-05-04 18:32:07 +0200

    third commit

--- pages/account/teori/index.js
+++ pages/account/teori/index.js
@@ -15,7 +15,7 @@ export default function Teori({ subjects }) {
       <section className={scss.teoriInner}>
         <h1>Teorilære</h1>
         <p>
-          Importantly, Next.js lets you choose which pre-rendering form you'd like to use for each page. You can create a "hybrid" Next.js app by using Static Generation for most pages and using Server-side Rendering for others.<br/><br/>
+          Importantly, Next.js lets you choose which pre-rendering form you&apos;d like to use for each page. You can create a &quot;hybrid&quot; Next.js app by using Static Generation for most pages and using Server-side Rendering for others.<br/><br/>
           We recommend using Static Generation over Server-side Rendering for performance reasons. Statically generated pages can be cached by CDN with no extra configuration to boost performance. However, in some cases, Server-side Rendering might be the only option.<br/><br/>
           You can also use Client-side Rendering along with Static Generation or Server-side Rendering. That means some parts of a page can be rendered entirely by client side JavaScript. To learn more, take a look at the Data Fetching documentation.
         </p>
