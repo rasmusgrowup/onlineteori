532d1e23fe499727f82c368417a62854e3f9b069
Rasmus Andersen <rasmus@growupstudio.dk>
2022-09-24 15:50:08 +0200

    proever page

--- pages/dashboard/proever/index.js
+++ pages/dashboard/proever/index.js
@@ -52,11 +52,24 @@ function NewTest() {
     <div className={style.newTest}>
       <p><strong>Start en ny prøve.</strong> Du har taget 10 ud af 15 mulige prøver.</p>
       <div className={style.plusContainer}>
-        <Image src={Plus} layout='responsive' quality='100'/>
+        <Image src={Plus} layout='responsive' quality='100' priority='true'/>
       </div>
     </div>
   )
 }
+
+const proever = [
+  ['Bestået', '97/100', '11/10/22'],
+  ['Bestået', '98/100', '11/10/22'],
+  ['Ikke bestået', '90/100', '11/10/22'],
+  ['Bestået', '95/100', '11/10/22'],
+  ['Ikke bestået', '82/100', '11/10/22'],
+  ['Ikke bestået', '92/100', '11/10/22'],
+  ['Bestået', '83/100', '11/10/22'],
+  ['Ikke bestået', '74/100', '11/10/22'],
+  ['Ikke bestået', '73/100', '11/10/22'],
+  ['Ikke bestået', '73/100', '11/10/22']
+];
 
 export default function Proever({ user }) {
 	return (
@@ -67,6 +80,18 @@ export default function Proever({ user }) {
           <User navn={user.name} src={user.userPic.url}/>
         </header>
         <NewTest />
+        <div className={style.testContainer}>
+          <h1>Seneste prøver</h1>
+          <div className={style.testList}>
+            {proever.map((proeve, i) => (
+              <div className={ proeve[0] == 'Bestået' ? `${style.testItem} ${style.passed}` : `${style.testItem}`}>
+                <div className={style.result}>{proeve[0]}</div>
+                <div className={style.score}>{proeve[1]}</div>
+                <div className={style.date}>{proeve[2]}</div>
+              </div>
+            ))}
+          </div>
+        </div>
         <Footer />
       </section>
     </>
